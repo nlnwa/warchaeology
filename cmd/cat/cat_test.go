5e58a440e9d6ac4d06378bce04cf69e4156836d3
Trym bremnes <trym.bremnes@gmail.com>
2024-03-11 09:50:22 +0100

    dummy benchmark test

---
+++ cmd/cat/cat_test.go
@@ -0,0 +1,14 @@
+package cat
+
+import (
+	"testing"
+	"time"
+)
+
+func BenchmarkDummy(b *testing.B) {
+	// This is a dummy test, it should be replaced with something more
+	// meaningful in a later commit
+	for i := 0; i < b.N; i++ {
+		time.Sleep(1 * time.Second)
+	}
+}
