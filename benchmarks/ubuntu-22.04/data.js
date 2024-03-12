49ff623f53c172c0f200bf5b607b9d126093a8b8
github-action-benchmark <github@users.noreply.github.com>
2024-03-12 10:11:21 +0000

    add Go Benchmark (go) benchmark result for 5f11080fbb088c60d82805ba94b3585a850277a9

---
+++ benchmarks/ubuntu-22.04/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1710238281690,
+  "repoUrl": "https://github.com/nlnwa/warchaeology",
+  "entries": {
+    "Go Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "trym.bremnes@gmail.com",
+            "name": "Trym Bremnes",
+            "username": "trym-b"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5f11080fbb088c60d82805ba94b3585a850277a9",
+          "message": "Merge pull request #91 from nlnwa/ci/add-benchmark-workflow\n\nCI: add benchmark workflow",
+          "timestamp": "2024-03-12T11:11:01+01:00",
+          "tree_id": "58d8a5b3250aa3d79f3112e6b542be14a915cf87",
+          "url": "https://github.com/nlnwa/warchaeology/commit/5f11080fbb088c60d82805ba94b3585a850277a9"
+        },
+        "date": 1710238281219,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkDummy",
+            "value": 190.8,
+            "unit": "ns/op",
+            "extra": "6426163 times\n4 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
