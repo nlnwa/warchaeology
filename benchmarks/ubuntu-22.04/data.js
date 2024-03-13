e926d63a003e17f16a53d287342d5079cf04d1c6
github-action-benchmark <github@users.noreply.github.com>
2024-03-13 11:48:26 +0000

    add Go Benchmark (go) benchmark result for ab32b5f0340bc6b44ded32960c8fe1dbbcefe29c

--- benchmarks/ubuntu-22.04/data.js
+++ benchmarks/ubuntu-22.04/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1710238281690,
+  "lastUpdate": 1710330506285,
   "repoUrl": "https://github.com/nlnwa/warchaeology",
   "entries": {
     "Go Benchmark": [
@@ -30,6 +30,36 @@ window.BENCHMARK_DATA = {
             "value": 190.8,
             "unit": "ns/op",
             "extra": "6426163 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "trym.bremnes@gmail.com",
+            "name": "Trym Bremnes",
+            "username": "trym-b"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ab32b5f0340bc6b44ded32960c8fe1dbbcefe29c",
+          "message": "Merge pull request #92 from nlnwa/ci/auto-commit-benchmark-results-to-dedicated-branch\n\nci: push benchmark results to dedicated branch",
+          "timestamp": "2024-03-13T12:48:03+01:00",
+          "tree_id": "710d3b1704769d90b3517ec58380b6695f6c3616",
+          "url": "https://github.com/nlnwa/warchaeology/commit/ab32b5f0340bc6b44ded32960c8fe1dbbcefe29c"
+        },
+        "date": 1710330505636,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkDummy",
+            "value": 189.2,
+            "unit": "ns/op",
+            "extra": "6149486 times\n4 procs"
           }
         ]
       }
