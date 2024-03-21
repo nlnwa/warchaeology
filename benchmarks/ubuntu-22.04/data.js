5a8e272c4a7a548d190dfac661c6a1bef7f4eb60
github-action-benchmark <github@users.noreply.github.com>
2024-03-21 12:59:56 +0000

    add Go Benchmark (go) benchmark result for af8a95b98503d8fe95b4a871b75e9257bb7e7b71

--- benchmarks/ubuntu-22.04/data.js
+++ benchmarks/ubuntu-22.04/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1711024804785,
+  "lastUpdate": 1711025996443,
   "repoUrl": "https://github.com/nlnwa/warchaeology",
   "entries": {
     "Go Benchmark": [
@@ -270,6 +270,36 @@ window.BENCHMARK_DATA = {
             "value": 190.9,
             "unit": "ns/op",
             "extra": "6356883 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "trym.bremnes@gmail.com",
+            "name": "Trym Bremnes",
+            "username": "trym-b"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "af8a95b98503d8fe95b4a871b75e9257bb7e7b71",
+          "message": "Merge pull request #103 from nlnwa/revert/mac-os-benchmark\n\nRevert \"ci: enable benchmarking for MacOS\"",
+          "timestamp": "2024-03-21T13:59:32+01:00",
+          "tree_id": "1d1c41c0c770d428da5e264d94aeefffa90f5981",
+          "url": "https://github.com/nlnwa/warchaeology/commit/af8a95b98503d8fe95b4a871b75e9257bb7e7b71"
+        },
+        "date": 1711025995942,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkDummy",
+            "value": 188.8,
+            "unit": "ns/op",
+            "extra": "6369168 times\n4 procs"
           }
         ]
       }
