acf33f2f90ce6af3cdd968aeb2c4cba96285b609
Trym Bremnes <trym.bremnes@gmail.com>
2024-03-27 10:12:09 +0100

    refactor: rename variables in `listwidget.go`
    This commit renames the variables in
    `listwidget.go` to make the code more readable.
    There are no logic changes.

--- cmd/console/listwidget.go
+++ cmd/console/listwidget.go
@@ -20,8 +20,8 @@ type ListWidget struct {
 	prev                string
 	next                string
 	selected            int
-	selectFunc          func(g *gocui.Gui, widget *ListWidget)
-	populateRecordsFunc func(g *gocui.Gui, ctx context.Context, finishedCb func(), widget *ListWidget, data interface{})
+	selectFunc          func(gui *gocui.Gui, widget *ListWidget)
+	populateRecordsFunc func(gui *gocui.Gui, ctx context.Context, finishedCb func(), widget *ListWidget, data interface{})
 	filterFunc          func(interface{}) bool
 	records             []interface{}
 	filteredRecords     []interface{}
@@ -32,8 +32,8 @@ type ListWidget struct {
 }
 
 func NewListWidget(name string, prev, next string,
-	selectFunc func(g *gocui.Gui, widget *ListWidget),
-	populateRecordsFunc func(g *gocui.Gui, ctx context.Context, finishedCb func(), widget *ListWidget, data interface{})) *ListWidget {
+	selectFunc func(gui *gocui.Gui, widget *ListWidget),
+	populateRecordsFunc func(gui *gocui.Gui, ctx context.Context, finishedCb func(), widget *ListWidget, data interface{})) *ListWidget {
 
 	return &ListWidget{
 		name:                name,
@@ -45,277 +45,277 @@ func NewListWidget(name string, prev, next string,
 	}
 }
 
-func (w *ListWidget) Init(g *gocui.Gui, data interface{}) {
-	if w.cancelFunc != nil {
-		w.cancelRefreshFunc()
-		w.cancelRefreshFunc = nil
-		w.cancelFunc()
-		w.cancelFunc = nil
-		<-w.finished.Done()
-		w.finished = nil
+func (widgetList *ListWidget) Init(gui *gocui.Gui, data interface{}) {
+	if widgetList.cancelFunc != nil {
+		widgetList.cancelRefreshFunc()
+		widgetList.cancelRefreshFunc = nil
+		widgetList.cancelFunc()
+		widgetList.cancelFunc = nil
+		<-widgetList.finished.Done()
+		widgetList.finished = nil
 	}
 	ctx, cancel := context.WithCancel(context.Background())
-	w.cancelFunc = cancel
+	widgetList.cancelFunc = cancel
 	var finishedCb func()
 	var refreshCtx context.Context
-	refreshCtx, w.cancelRefreshFunc = context.WithCancel(context.Background())
-	w.finished, finishedCb = context.WithCancel(context.Background())
-	w.records = nil
-	w.filteredRecords = nil
-	w.selected = -1
+	refreshCtx, widgetList.cancelRefreshFunc = context.WithCancel(context.Background())
+	widgetList.finished, finishedCb = context.WithCancel(context.Background())
+	widgetList.records = nil
+	widgetList.filteredRecords = nil
+	widgetList.selected = -1
 	go func() {
-		w.populateRecordsFunc(g, ctx, finishedCb, w, data)
+		widgetList.populateRecordsFunc(gui, ctx, finishedCb, widgetList, data)
 	}()
-	w.update(g, refreshCtx, w.finished)
+	widgetList.update(gui, refreshCtx, widgetList.finished)
 }
 
-func (w *ListWidget) Layout(g *gocui.Gui) error {
-	if v, err := g.SetView(w.name, 0, 0, 1, 1, 0); err != nil {
+func (widgetList *ListWidget) Layout(gui *gocui.Gui) error {
+	if view, err := gui.SetView(widgetList.name, 0, 0, 1, 1, 0); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
-		v.FgColor = gocui.ColorGreen
-		v.BgColor = gocui.ColorDefault
-		v.SelBgColor = gocui.ColorWhite
-		v.SelFgColor = gocui.ColorBlack
-		v.Highlight = false
-		v.Autoscroll = false
-		v.Title = w.name
+		view.FgColor = gocui.ColorGreen
+		view.BgColor = gocui.ColorDefault
+		view.SelBgColor = gocui.ColorWhite
+		view.SelFgColor = gocui.ColorBlack
+		view.Highlight = false
+		view.Autoscroll = false
+		view.Title = widgetList.name
 	}
-	if err := g.SetKeybinding(w.name, gocui.KeyArrowDown, gocui.ModNone, w.cursorDown); err != nil {
+	if err := gui.SetKeybinding(widgetList.name, gocui.KeyArrowDown, gocui.ModNone, widgetList.cursorDown); err != nil {
 		return err
 	}
-	if err := g.SetKeybinding(w.name, gocui.KeyArrowUp, gocui.ModNone, w.cursorUp); err != nil {
+	if err := gui.SetKeybinding(widgetList.name, gocui.KeyArrowUp, gocui.ModNone, widgetList.cursorUp); err != nil {
 		return err
 	}
-	if err := g.SetKeybinding(w.name, gocui.KeyHome, gocui.ModNone, func(gui *gocui.Gui, view *gocui.View) error {
+	if err := gui.SetKeybinding(widgetList.name, gocui.KeyHome, gocui.ModNone, func(guiInner *gocui.Gui, view *gocui.View) error {
 		if view != nil {
-			return w.selectLine(g, view, 0)
+			return widgetList.selectLine(gui, view, 0)
 		}
 		return nil
 	}); err != nil {
 		return err
 	}
-	if err := g.SetKeybinding(w.name, gocui.KeyEnd, gocui.ModNone, func(gui *gocui.Gui, view *gocui.View) error {
+	if err := gui.SetKeybinding(widgetList.name, gocui.KeyEnd, gocui.ModNone, func(guiInner *gocui.Gui, view *gocui.View) error {
 		if view != nil {
-			return w.selectLine(g, view, len(w.filteredRecords)-1)
+			return widgetList.selectLine(gui, view, len(widgetList.filteredRecords)-1)
 		}
 		return nil
 	}); err != nil {
 		return err
 	}
-	if err := g.SetKeybinding(w.name, gocui.KeyPgdn, gocui.ModNone, func(gui *gocui.Gui, view *gocui.View) error {
+	if err := gui.SetKeybinding(widgetList.name, gocui.KeyPgdn, gocui.ModNone, func(guiInner *gocui.Gui, view *gocui.View) error {
 		if view != nil {
-			_, h := view.Size()
-			h--
-			return w.selectLine(g, view, w.selected+h)
+			_, height := view.Size()
+			height--
+			return widgetList.selectLine(gui, view, widgetList.selected+height)
 		}
 		return nil
 	}); err != nil {
 		return err
 	}
-	if err := g.SetKeybinding(w.name, gocui.KeyPgup, gocui.ModNone, func(gui *gocui.Gui, view *gocui.View) error {
+	if err := gui.SetKeybinding(widgetList.name, gocui.KeyPgup, gocui.ModNone, func(guiInner *gocui.Gui, view *gocui.View) error {
 		if view != nil {
-			_, h := view.Size()
-			h--
-			return w.selectLine(g, view, w.selected-h)
+			_, height := view.Size()
+			height--
+			return widgetList.selectLine(gui, view, widgetList.selected-height)
 		}
 		return nil
 	}); err != nil {
 		return err
 	}
-	if err := g.SetKeybinding(w.name, 'f', gocui.ModNone, w.search); err != nil {
+	if err := gui.SetKeybinding(widgetList.name, 'f', gocui.ModNone, widgetList.search); err != nil {
 		return err
 	}
-	if err := g.SetKeybinding(w.name, gocui.KeyEnter, gocui.ModNone, w.nextView); err != nil {
+	if err := gui.SetKeybinding(widgetList.name, gocui.KeyEnter, gocui.ModNone, widgetList.nextView); err != nil {
 		return err
 	}
-	if err := g.SetKeybinding(w.name, gocui.KeyEsc, gocui.ModNone, w.prevView); err != nil {
+	if err := gui.SetKeybinding(widgetList.name, gocui.KeyEsc, gocui.ModNone, widgetList.prevView); err != nil {
 		return err
 	}
-	if err := g.SetKeybinding(w.name, gocui.MouseLeft, gocui.ModNone, w.currentView); err != nil {
+	if err := gui.SetKeybinding(widgetList.name, gocui.MouseLeft, gocui.ModNone, widgetList.currentView); err != nil {
 		return err
 	}
-	if err := g.SetKeybinding(w.name, gocui.MouseRelease, gocui.ModNone, func(gui *gocui.Gui, view *gocui.View) error {
+	if err := gui.SetKeybinding(widgetList.name, gocui.MouseRelease, gocui.ModNone, func(guiInner *gocui.Gui, view *gocui.View) error {
 		return nil
 	}); err != nil {
 		return err
 	}
-	if err := g.SetKeybinding(w.name, gocui.MouseWheelDown, gocui.ModNone, w.cursorDown); err != nil {
+	if err := gui.SetKeybinding(widgetList.name, gocui.MouseWheelDown, gocui.ModNone, widgetList.cursorDown); err != nil {
 		return err
 	}
-	if err := g.SetKeybinding(w.name, gocui.MouseWheelUp, gocui.ModNone, w.cursorUp); err != nil {
+	if err := gui.SetKeybinding(widgetList.name, gocui.MouseWheelUp, gocui.ModNone, widgetList.cursorUp); err != nil {
 		return err
 	}
 	return nil
 }
 
-func (w *ListWidget) search(g *gocui.Gui, parent *gocui.View) error {
-	maxX, maxY := g.Size()
-	var v *gocui.View
+func (widgetList *ListWidget) search(gui *gocui.Gui, parent *gocui.View) error {
+	maxX, maxY := gui.Size()
+	var view *gocui.View
 	var err error
-	if v, err = g.SetView("search", maxX/2-35, maxY/2-1, maxX/2+35, maxY/2+1, 0); err != nil {
+	if view, err = gui.SetView("search", maxX/2-35, maxY/2-1, maxX/2+35, maxY/2+1, 0); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
-		v.FgColor = gocui.ColorDefault
-		v.BgColor = gocui.ColorDefault
-		v.SelBgColor = gocui.ColorWhite
-		v.SelFgColor = gocui.ColorBlack
-		v.Highlight = false
-		v.Autoscroll = false
-		v.Title = "Find in " + parent.Title
-		v.FrameRunes = []rune{'═', '║', '╔', '╗', '╚', '╝'}
-		v.Editable = true
-		v.KeybindOnEdit = false
-		v.Editor = &SearchEditor{g, w, parent}
-		g.Cursor = true
+		view.FgColor = gocui.ColorDefault
+		view.BgColor = gocui.ColorDefault
+		view.SelBgColor = gocui.ColorWhite
+		view.SelFgColor = gocui.ColorBlack
+		view.Highlight = false
+		view.Autoscroll = false
+		view.Title = "Find in " + parent.Title
+		view.FrameRunes = []rune{'═', '║', '╔', '╗', '╚', '╝'}
+		view.Editable = true
+		view.KeybindOnEdit = false
+		view.Editor = &SearchEditor{gui, widgetList, parent}
+		gui.Cursor = true
 	}
 	state.modalView = "search"
 	return nil
 }
 
 type SearchEditor struct {
-	g      *gocui.Gui
-	w      *ListWidget
-	parent *gocui.View
+	gui        *gocui.Gui
+	widgetList *ListWidget
+	parent     *gocui.View
 }
 
-func (s *SearchEditor) Edit(v *gocui.View, key gocui.Key, ch rune, mod gocui.Modifier) {
-	if ch != 0 && mod == 0 {
-		v.EditWrite(ch)
-		_ = s.w.searchLine(s.g, s.parent, v.ViewBuffer())
+func (searchEditor *SearchEditor) Edit(view *gocui.View, key gocui.Key, characterAsInteger rune, modifier gocui.Modifier) {
+	if characterAsInteger != 0 && modifier == 0 {
+		view.EditWrite(characterAsInteger)
+		_ = searchEditor.widgetList.searchLine(searchEditor.gui, searchEditor.parent, view.ViewBuffer())
 		return
 	}
 
 	switch key {
 	case gocui.KeySpace:
-		v.EditWrite(' ')
-		_ = s.w.searchLine(s.g, s.parent, v.ViewBuffer())
+		view.EditWrite(' ')
+		_ = searchEditor.widgetList.searchLine(searchEditor.gui, searchEditor.parent, view.ViewBuffer())
 	case gocui.KeyBackspace, gocui.KeyBackspace2:
-		v.EditDelete(true)
-		_ = s.w.searchLine(s.g, s.parent, v.ViewBuffer())
+		view.EditDelete(true)
+		_ = searchEditor.widgetList.searchLine(searchEditor.gui, searchEditor.parent, view.ViewBuffer())
 	case gocui.KeyDelete:
-		v.EditDelete(false)
-		_ = s.w.searchLine(s.g, s.parent, v.ViewBuffer())
+		view.EditDelete(false)
+		_ = searchEditor.widgetList.searchLine(searchEditor.gui, searchEditor.parent, view.ViewBuffer())
 	case gocui.KeyInsert:
-		v.Overwrite = !v.Overwrite
+		view.Overwrite = !view.Overwrite
 	case gocui.KeyArrowLeft:
-		v.MoveCursor(-1, 0)
+		view.MoveCursor(-1, 0)
 	case gocui.KeyArrowRight:
-		v.MoveCursor(1, 0)
+		view.MoveCursor(1, 0)
 	case gocui.KeyEsc:
 		// If not here the esc key will act like the KeySpace
 		fallthrough
 	case gocui.KeyEnter:
-		_ = s.g.DeleteView(v.Name())
+		_ = searchEditor.gui.DeleteView(view.Name())
 		state.modalView = ""
-		s.g.Cursor = false
+		searchEditor.gui.Cursor = false
 	default:
 	}
 }
 
-func (w *ListWidget) prevView(g *gocui.Gui, v *gocui.View) error {
-	state.curView = w.prev
+func (widgetList *ListWidget) prevView(gui *gocui.Gui, view *gocui.View) error {
+	state.curView = widgetList.prev
 	return nil
 }
 
-func (w *ListWidget) nextView(g *gocui.Gui, v *gocui.View) error {
-	state.curView = w.next
+func (widgetList *ListWidget) nextView(gui *gocui.Gui, view *gocui.View) error {
+	state.curView = widgetList.next
 	return nil
 }
 
-func (w *ListWidget) currentView(g *gocui.Gui, v *gocui.View) error {
-	state.curView = v.Name()
-	_, oy := v.Origin()
-	_, cy := v.Cursor()
+func (widgetList *ListWidget) currentView(gui *gocui.Gui, view *gocui.View) error {
+	state.curView = view.Name()
+	_, oy := view.Origin()
+	_, cy := view.Cursor()
 	newSelect := cy + oy
-	_ = w.selectLine(g, v, newSelect)
+	_ = widgetList.selectLine(gui, view, newSelect)
 	return nil
 }
 
-func (w *ListWidget) cursorDown(g *gocui.Gui, v *gocui.View) error {
-	if v != nil {
-		_ = w.selectLine(g, v, w.selected+1)
+func (widgetList *ListWidget) cursorDown(gui *gocui.Gui, view *gocui.View) error {
+	if view != nil {
+		_ = widgetList.selectLine(gui, view, widgetList.selected+1)
 	}
 	return nil
 }
 
-func (w *ListWidget) cursorUp(g *gocui.Gui, v *gocui.View) error {
-	if v != nil {
-		_ = w.selectLine(g, v, w.selected-1)
+func (widgetList *ListWidget) cursorUp(gui *gocui.Gui, view *gocui.View) error {
+	if view != nil {
+		_ = widgetList.selectLine(gui, view, widgetList.selected-1)
 	}
 	return nil
 }
 
-func (w *ListWidget) searchLine(g *gocui.Gui, v *gocui.View, s string) error {
-	if v != nil {
-		for i, id := range v.ViewBufferLines() {
-			if strings.Contains(id, s) {
-				return w.selectLine(g, v, i)
+func (widgetList *ListWidget) searchLine(gui *gocui.Gui, view *gocui.View, targetString string) error {
+	if view != nil {
+		for line, id := range view.ViewBufferLines() {
+			if strings.Contains(id, targetString) {
+				return widgetList.selectLine(gui, view, line)
 			}
 		}
 	}
 	return nil
 }
 
-func (w *ListWidget) selectLine(g *gocui.Gui, v *gocui.View, selected int) error {
-	if v != nil {
-		ox, oy := v.Origin()
-		_, h := v.Size()
+func (widgetList *ListWidget) selectLine(gui *gocui.Gui, view *gocui.View, selected int) error {
+	if view != nil {
+		originX, originY := view.Origin()
+		_, height := view.Size()
 		if selected < 0 {
 			selected = 0
 		}
-		if selected >= v.ViewLinesHeight()-1 {
-			selected = v.ViewLinesHeight() - 2
+		if selected >= view.ViewLinesHeight()-1 {
+			selected = view.ViewLinesHeight() - 2
 		}
 
 		// Do nothing if no movement or content is empty
-		if selected == w.selected || v.ViewLinesHeight() == 0 {
+		if selected == widgetList.selected || view.ViewLinesHeight() == 0 {
 			return nil
 		}
 
-		if w.selected != -1 {
-			_ = v.SetLine(w.selected, fmt.Sprintf("%s", w.filteredRecords[w.selected]))
+		if widgetList.selected != -1 {
+			_ = view.SetLine(widgetList.selected, fmt.Sprintf("%s", widgetList.filteredRecords[widgetList.selected]))
 		}
-		w.selected = selected
-		_ = v.SetHighlight(w.selected, true)
+		widgetList.selected = selected
+		_ = view.SetHighlight(widgetList.selected, true)
 
-		cy := w.selected - oy
-		if cy < 0 {
-			oy += cy
-			cy = 0
-		} else if cy >= h {
-			oy += (cy - h) + 1
-			cy -= (cy - h) + 1
+		cursorPositionY := widgetList.selected - originY
+		if cursorPositionY < 0 {
+			originY += cursorPositionY
+			cursorPositionY = 0
+		} else if cursorPositionY >= height {
+			originY += (cursorPositionY - height) + 1
+			cursorPositionY -= (cursorPositionY - height) + 1
 		}
 
-		if err := v.SetOrigin(ox, oy); err != nil {
+		if err := view.SetOrigin(originX, originY); err != nil {
 			return err
 		}
-		if err := v.SetCursor(0, cy); err != nil {
+		if err := view.SetCursor(0, cursorPositionY); err != nil {
 			return err
 		}
 
-		if l, err := v.Line(w.selected); err != nil || l == "" {
+		if line, err := view.Line(widgetList.selected); err != nil || line == "" {
 			return nil
 		} else {
-			v.Subtitle = fmt.Sprintf("%d/%d", w.selected+1, len(w.filteredRecords))
-			if len(w.filteredRecords) > 0 {
-				w.selectFunc(g, w)
+			view.Subtitle = fmt.Sprintf("%d/%d", widgetList.selected+1, len(widgetList.filteredRecords))
+			if len(widgetList.filteredRecords) > 0 {
+				widgetList.selectFunc(gui, widgetList)
 			}
 		}
 	}
 	return nil
 }
 
-func (w *ListWidget) update(g *gocui.Gui, ctx context.Context, finished context.Context) {
+func (widgetList *ListWidget) update(gui *gocui.Gui, ctx context.Context, finished context.Context) {
 	time.Sleep(100 * time.Millisecond)
-	v, err := g.View(w.name)
+	view, err := gui.View(widgetList.name)
 	if err != nil {
 		panic(err)
 	}
-	v.Clear()
+	view.Clear()
 
 	go func() {
 		defer func() {
@@ -324,32 +324,32 @@ func (w *ListWidget) update(g *gocui.Gui, ctx context.Context, finished context.
 			}
 		}()
 
-		l := 0
+		length := 0
 		for {
 			select {
 			case <-ctx.Done():
 				<-finished.Done()
 				return
 			case <-finished.Done():
-				if l < len(w.records) {
-					rec := w.records[l:]
-					w.upd(g, ctx, rec)
+				if length < len(widgetList.records) {
+					rec := widgetList.records[length:]
+					widgetList.upd(gui, ctx, rec)
 				}
 				return
 			default:
-				if l < len(w.records) {
-					rec := w.records[l:]
-					l = l + len(rec)
-					w.upd(g, ctx, rec)
+				if length < len(widgetList.records) {
+					rec := widgetList.records[length:]
+					length = length + len(rec)
+					widgetList.upd(gui, ctx, rec)
 				}
 			}
 		}
 	}()
 }
 
-func (w *ListWidget) upd(g *gocui.Gui, ctx context.Context, rec []interface{}) {
-	g.UpdateAsync(func(gui *gocui.Gui) error {
-		v, err := gui.View(w.name)
+func (widgetList *ListWidget) upd(gui *gocui.Gui, ctx context.Context, rec []interface{}) {
+	gui.UpdateAsync(func(guiInner *gocui.Gui) error {
+		view, err := guiInner.View(widgetList.name)
 		if err != nil {
 			return err
 		}
@@ -359,55 +359,55 @@ func (w *ListWidget) upd(g *gocui.Gui, ctx context.Context, rec []interface{}) {
 			case <-ctx.Done():
 				return nil
 			default:
-				if w.filterFunc == nil || w.filterFunc(r) {
-					w.filteredRecords = append(w.filteredRecords, r)
-					fmt.Fprintf(v, "%s\n", r)
-					if len(w.filteredRecords) == 1 {
-						_ = w.selectLine(g, v, 0)
-						w.selectFunc(g, w)
+				if widgetList.filterFunc == nil || widgetList.filterFunc(r) {
+					widgetList.filteredRecords = append(widgetList.filteredRecords, r)
+					fmt.Fprintf(view, "%s\n", r)
+					if len(widgetList.filteredRecords) == 1 {
+						_ = widgetList.selectLine(gui, view, 0)
+						widgetList.selectFunc(gui, widgetList)
 					}
 				}
 			}
 		}
-		v.Subtitle = fmt.Sprintf("%d/%d", w.selected+1, len(w.filteredRecords))
+		view.Subtitle = fmt.Sprintf("%d/%d", widgetList.selected+1, len(widgetList.filteredRecords))
 		return nil
 	})
 }
 
-func (w *ListWidget) refreshFilter(g *gocui.Gui, v *gocui.View) error {
-	if w.cancelRefreshFunc != nil {
-		w.cancelRefreshFunc()
-		w.cancelRefreshFunc = nil
+func (widgetList *ListWidget) refreshFilter(gui *gocui.Gui, view *gocui.View) error {
+	if widgetList.cancelRefreshFunc != nil {
+		widgetList.cancelRefreshFunc()
+		widgetList.cancelRefreshFunc = nil
 	}
 
 	// Skip filtering if records is empty and no read operation is in progress
-	if len(w.records) == 0 && w.finished == nil {
+	if len(widgetList.records) == 0 && widgetList.finished == nil {
 		return nil
 	}
 
 	var ctx context.Context
-	ctx, w.cancelRefreshFunc = context.WithCancel(context.Background())
-	v.Clear()
-	w.filteredRecords = nil
-	w.selected = -1
+	ctx, widgetList.cancelRefreshFunc = context.WithCancel(context.Background())
+	view.Clear()
+	widgetList.filteredRecords = nil
+	widgetList.selected = -1
 
 	go func() {
-		l := 0
+		length := 0
 		for {
 			select {
 			case <-ctx.Done():
 				return
-			case <-w.finished.Done():
-				if l < len(w.records) {
-					rec := w.records[l:]
-					w.upd(g, ctx, rec)
+			case <-widgetList.finished.Done():
+				if length < len(widgetList.records) {
+					rec := widgetList.records[length:]
+					widgetList.upd(gui, ctx, rec)
 				}
 				return
 			default:
-				if l < len(w.records) {
-					rec := w.records[l:]
-					l = l + len(rec)
-					w.upd(g, ctx, rec)
+				if length < len(widgetList.records) {
+					rec := widgetList.records[length:]
+					length = length + len(rec)
+					widgetList.upd(gui, ctx, rec)
 				}
 			}
 		}
