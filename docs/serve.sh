2fef93f476c154692c9966ec77aa7136fbc58da8
Marius André Elsfjordstrand Beck <marius.beck@nb.no>
2025-02-27 15:47:21 +0100

    fix: broken download links in documentation for RPM and DEB packages

--- docs/serve.sh
+++ docs/serve.sh
@@ -7,4 +7,4 @@ set -o errexit -o nounset -o pipefail
 REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd -P)"
 cd "${REPO_ROOT}"
 
-GITHUB_REF_NAME=next HUGO_BASEURL=http://localhost/ hugo serve --buildDrafts --source docs
+GITHUB_REF_NAME=${GITHUB_REF_NAME:-next} HUGO_BASEURL=http://localhost/ hugo serve --buildDrafts --source docs
