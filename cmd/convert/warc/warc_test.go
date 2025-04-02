a380fc1440508179185723b4569b3e1c02e5ae91
Marius André Elsfjordstrand Beck <marius.beck@nb.no>
2025-04-02 13:49:16 +0200

    fix: use record options from flags in warc-to-warc conversion
    This commit makes sure to use the record option flags and also makes sure the
    recordBuilder uses the same options. Otherwise the recordBuilder may err when
    e.g. lax host parsing is succesful when reading but not building the new record.

--- cmd/convert/warc/warc_test.go
+++ cmd/convert/warc/warc_test.go
@@ -4,6 +4,8 @@ import (
 	"path/filepath"
 	"testing"
 
+	"github.com/nlnwa/gowarc/v2"
+	"github.com/nlnwa/warchaeology/v4/internal/warc"
 	"github.com/nlnwa/warchaeology/v4/internal/warcwriterconfig"
 	"github.com/spf13/afero"
 )
@@ -16,6 +18,7 @@ var testFiles = map[string]string{
 	"empty":              filepath.Join(testDataDir, "warc", "empty.warc"),
 	"single-record":      filepath.Join(testDataDir, "warc", "single-record.warc"),
 	"samsung-with-error": filepath.Join(testDataDir, "warc", "samsung-with-error", "rec-33318048d933-20240317162652059-0.warc.gz"),
+	"convert":            filepath.Join(testDataDir, "warc", "convert.warc"),
 }
 
 func TestConvertWarcFile(t *testing.T) {
@@ -66,6 +69,106 @@ func TestConvertWarcFile(t *testing.T) {
 				for _, err := range result.Errors() {
 					t.Error(err)
 				}
+			}
+		})
+	}
+}
+
+func TestRepairWarcFile(t *testing.T) {
+	// TODO add tests for every type of error that can be repaired and some that cannot
+	tests := []struct {
+		name         string
+		wantNrErrors int64
+	}{
+		{
+			name:         testFiles["convert"],
+			wantNrErrors: 5,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			filename, err := filepath.Abs(tt.name)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			outDir := t.TempDir()
+
+			// Create a WARC writer config
+			warcWriterConfig, err := warcwriterconfig.New("test",
+				warcwriterconfig.WithBufferTmpDir(t.TempDir()),
+				warcwriterconfig.WithOutDir(outDir),
+				warcwriterconfig.WithCompress(false),
+				warcwriterconfig.WithOneToOneWriter(true),
+			)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer warcWriterConfig.Close()
+
+			// Set the WARC record options
+			policy := gowarc.ErrWarn
+			repair := true
+			warcRecordOptions := []gowarc.WarcRecordOption{
+				gowarc.WithSyntaxErrorPolicy(policy),
+				gowarc.WithSpecViolationPolicy(policy),
+				gowarc.WithAddMissingDigest(repair),
+				gowarc.WithFixSyntaxErrors(repair),
+				gowarc.WithFixDigest(repair),
+				gowarc.WithAddMissingContentLength(repair),
+				gowarc.WithAddMissingRecordId(repair),
+				gowarc.WithFixContentLength(repair),
+				gowarc.WithFixWarcFieldsBlockErrors(repair),
+			}
+
+			o := &ConvertWarcOptions{
+				WarcWriterConfig:  warcWriterConfig,
+				WarcRecordOptions: warcRecordOptions,
+			}
+
+			// Convert the WARC file
+			result, err := o.handleFile(afero.NewOsFs(), filename)
+			if err != nil {
+				t.Fatal(err)
+			}
+			for _, err := range result.Errors() {
+				t.Logf("Found error during conversion: %v", err)
+			}
+			if result.ErrorCount() != tt.wantNrErrors {
+				t.Errorf("Expected %d errors, got %d", tt.wantNrErrors, result.ErrorCount())
+			}
+
+			// Validate the repaired WARC file
+			outFile := filepath.Join(outDir, filepath.Base(filename))
+			policy = gowarc.ErrWarn
+			repair = false
+			wr, err := gowarc.NewWarcFileReader(outFile, 0,
+				gowarc.WithSyntaxErrorPolicy(policy),
+				gowarc.WithSpecViolationPolicy(policy),
+				gowarc.WithAddMissingDigest(repair),
+				gowarc.WithFixSyntaxErrors(repair),
+				gowarc.WithFixDigest(repair),
+				gowarc.WithAddMissingContentLength(repair),
+				gowarc.WithAddMissingRecordId(repair),
+				gowarc.WithFixContentLength(repair),
+				gowarc.WithFixWarcFieldsBlockErrors(repair),
+			)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer func() { _ = wr.Close() }()
+
+			for record, err := range warc.Records(wr, nil, 0, 0) {
+				if err != nil {
+					t.Fatal(err)
+				}
+				for _, err = range *record.Validation {
+					if err != nil {
+						t.Error(err)
+					}
+				}
+				record.Close()
 			}
 		})
 	}
